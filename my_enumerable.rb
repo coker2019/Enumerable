a6da41c876cad1c065cf1498335523d5df6c6dcf
coker2019 <olabisi2k11@gmail.com>
2023-10-06 15:22:37 +0100

    update enumerable/fixed linters

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -3,14 +3,15 @@ module MyEnumerable
     each { |item| return false unless yield(item) }
     true
   end
+
   def any?
     each { |item| return true if yield(item) }
     false
   end
-  
+
   def filter
     result = []
     each { |item| result.push(item) if yield(item) }
     result
-    end
-  end
+  end
+end
