b09a89d3b3b800bdb847feae10da0d5c973fd506
coker2019 <olabisi2k11@gmail.com>
2023-10-06 06:21:26 +0100

    Implement Enumerable.

---
+++ my_list.rb
@@ -0,0 +1,28 @@
+require_relative 'my_enumerable'
+
+class MyList                
+    include MyEnumerable
+
+
+
+        def initialize(*items)
+         @list = items
+        end
+    end
+
+    def each(&block)
+        @list.each(&block)
+    end
+
+list = MyList.new(1, 2, 3, 4, 5, 6)
+
+puts(list.all? { |x| x < 7 })
+# true
+puts(list.all? { |e| e > 6 })
+# false
+puts(list.any? { |e| e == 3 })
+# true
+puts(list.any? { |e| e == 7 })
+# false
+print(list.filter(&:even?))
+# [2, 4, 6]
